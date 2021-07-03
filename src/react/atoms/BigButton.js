2223a6f9a60d80c1094d151f217a6c71878ae99a
marson parulian <marson.parulian@gmail.com>
2021-07-03 12:34:04 +1000

    init React prop types

--- src/react/atoms/BigButton.js
+++ src/react/atoms/BigButton.js
@@ -1,9 +1,15 @@
 // Big Buttons
 import React from "react";
+import PropTypes from "prop-types";
 
 const BigButton = (props) => {
     return (
         <button>{props.children}</button>
     )
 }
+
+BigButton.propTypes = {
+    children: PropTypes.string.isRequired
+}
+
 export default BigButton;
