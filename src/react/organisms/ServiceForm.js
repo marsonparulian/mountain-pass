e0274e6113aad177ffdb50bb7d0cb8ea5f4ddc79
marson parulian <marson.parulian@gmail.com>
2021-07-07 07:58:14 +1000

    setup component to save a service

--- src/react/organisms/ServiceForm.js
+++ src/react/organisms/ServiceForm.js
@@ -1,5 +1,6 @@
 // Service Form
 import React from "react";
+import propTypes from "prop-types";
 import Button from "../atoms/Button";
 import Input from "../atoms/Input";
 import InputRow from "../molecules/InputRow";
@@ -10,6 +11,7 @@ class ServiceForm extends React.Component {
         super(props);
         this.handleCancel = this.handleCancel.bind(this);
         this.updateFieldValue = this.updateFieldValue.bind(this);
+        this.handleSubmitForm = this.handleSubmitForm.bind(this);
     }
     // Handle `Cancel` button
     handleCancel() {
@@ -22,52 +24,68 @@ class ServiceForm extends React.Component {
             e.target.value
         );
     }
+    // Submit form
+    handleSubmitForm(e) {
+        e.preventDefault();
+
+        // Prepare data
+        const fields = this.props.serviceForm.fields;
+        const data = {
+            name: fields.name.value,
+            version: fields.version.value,
+        }
+        // action
+        this.props.savePost(data);
+    }
     render() {
         // Form fields
         const fields = this.props.serviceForm.fields;
         return (
             <div>
                 <h1>Add Service</h1>
-                <form>
+                <formmethod="POST"onSubmit={this.handleSubmitForm}>
 
-                    <InputRow
-                        labelText="Name"
-                        for="name">
-                        <Input
-                            name="name"
-                            id="name"
-                            value={fields.name.value}
-                            onChange={(e) => this.updateFieldValue("name", e)}
-                        />
-                    </InputRow>
+                <InputRow
+                    labelText="Name"
+                    for="name">
+                    <Input
+                        name="name"
+                        id="name"
+                        value={fields.name.value}
+                        onChange={(e) => this.updateFieldValue("name", e)}
+                    />
+                </InputRow>
 
-                    <InputRow
-                        labelText="Version"
-                        for="version">
-                        <Input
-                            name="version"
-                            id="version"
-                            value={fields.version.value}
-                            onChange={(e) => this.updateFieldValue("version", e)}
-                        />
-                    </InputRow>
+                <InputRow
+                    labelText="Version"
+                    for="version">
+                    <Input
+                        name="version"
+                        id="version"
+                        value={fields.version.value}
+                        onChange={(e) => this.updateFieldValue("version", e)}
+                    />
+                </InputRow>
 
 
-                    <div>
-                        <Button
-                            type="button"
-                            onClick={this.handleCancel}
-                        >
-                            Cancel
-                        </Button>
-                        <Button
-                            type="submit">
-                            Add Service
-                        </Button>
-                    </div>
+                <div>
+                    <Button
+                        type="button"
+                        onClick={this.handleCancel}
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        type="submit">
+                        Add Service
+                    </Button>
+                </div>
                 </form>
-            </div>
+            </div >
         )
     }
+}
+ServiceForm.propTypes = {
+    saveService: propTypes.func.isRequired,
 }
 export default ServiceForm;
--- src/redux/containers/ServiceFormContainer.js
+++ src/redux/containers/ServiceFormContainer.js
@@ -1,7 +1,7 @@
 // Connect <ServiceForm /> 
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { hideServiceForm } from "../actions/serviceActions";
+import { hideServiceForm, saveService } from "../actions/serviceActions";
 import { updateFieldValue } from "../actions/appActions";
 import ServiceForm from "../../react/organisms/ServiceForm";
 
@@ -13,6 +13,7 @@ const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
         handleCancel: hideServiceForm,
         updateFieldValue,
+        saveService,
     }, dispatch);
 }
 
