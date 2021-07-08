7a7ecac1132bbbd6e00744c09b41f2972dbb6b7c
marson parulian <marson.parulian@gmail.com>
2021-07-08 22:15:28 +1000

    redux/global state to indicate if a service has been saved

--- src/defaults/serviceFormDefaultState.js
+++ src/defaults/serviceFormDefaultState.js
@@ -1,8 +1,9 @@
 // Default state for Service form
 
 const serviceFormDefaultState = {
-    // Indicate if a form just has been successfully submitted
-    saveSuccess: false,
+    // Indicate the service has just been saved
+    // If not `false`, will have value object of key/value pairs of the saved service.
+    savedService: false,
     // Fields of the form
     fields: {
         name: {
