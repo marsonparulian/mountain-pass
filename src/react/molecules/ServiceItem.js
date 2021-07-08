2a3c7becd7de9802f6a8b4e3391bf7d306ca1a97
marson parulian <marson.parulian@gmail.com>
2021-07-09 09:01:00 +1000

    apply style to service list

--- src/react/molecules/ServiceItem.js
+++ src/react/molecules/ServiceItem.js
@@ -5,8 +5,8 @@ import PropTypes from "prop-types";
 const ServiceItem = (props) => {
 
     return (
-        <div >
-            <div>{props.name}</div>
+        <div className="service-item">
+            <h3>{props.name}</h3>
             <div>version : {props.version}</div>
         </div>
     )
